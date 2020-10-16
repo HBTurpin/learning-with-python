0a0aac88cf9dcb2a27491866bab19b8e3ee37151
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2020-10-16 22:21:44 +0100

    16102020 (id10-id13)

--- src/id10-summationofprimes.py
+++ src/id10-summationofprimes.py
@@ -1,9 +1,8 @@
-#The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-#Find the sum of all the primes below two million.
-
 import math
 
 def is_prime(n):
+    if n == 1:
+        return False
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
