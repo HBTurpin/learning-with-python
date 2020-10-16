0e41c3f1cb33f8dd5c3ffe063a23ada46652b7b3
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2020-10-16 22:21:57 +0100

    16102020 (id7)

--- src/id7-10001stprime.py
+++ src/id7-10001stprime.py
@@ -1,13 +1,27 @@
-#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-#What is the 10 001st prime number?
+def is_prime(number):
+    if number == 1:
+        return False
+    for j in range (2,number,1):
+        if number % j == 0:
+            return False
+    return True
 
+
+
+#Could defo be optimised by not not incrementing through every number...
 def get_prime_number(position):
-    return 0
+    prime_count = 0
+    prime_check = 2
+    while prime_count <= position:
+        if is_prime(prime_check):
+            prime_count += 1
+            print(str(prime_count)+":"+str(prime_check))
+        if prime_count == position:
+            return prime_check
+        prime_check += 1
 
 
-
-
-print(get_prime_number(1,100))
+print(get_prime_number(10001))
 
 
 
