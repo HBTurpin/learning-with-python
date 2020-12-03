f311a8cfea88a7ba15db9e9f5a89d5b701d1be2e
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2020-12-03 13:46:23 +0000

    adventofcode.com (day3)

---
+++ src/adventofcode2020/day3-toboggantrajectory.py
@@ -0,0 +1,29 @@
+
+with open('day3-input') as file:
+    entries = [line.rstrip() for line in file]
+
+# Part 1
+total_trees = 0
+for y in range(0, len(entries), 1):
+    x = (y * 3) % len(entries[0])
+    if entries[y][x] == "#":
+        total_trees += 1
+print(total_trees)
+
+#Part 2
+paths = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+result = 1
+for path in paths:
+    total_trees = 0
+    step = 0
+    while (step * path[1]) < len(entries):
+        y = (step * path[1])
+        x = (step * path[0]) % len(entries[y])
+        if entries[y][x] == "#":
+            total_trees += 1
+        step += 1
+    result *= total_trees
+print(result)
+
+
+
